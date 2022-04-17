ad2e8a1402265db5e789b8ae6ae8c225515ce59a
Manish Devrani <manishdevrani777@gmail.com>
2022-04-17 20:49:13 +0530

    absolute path added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import { Home } from 'pages';
+
 const App = () => (
   <div className="App">
     <h1>This is div</h1>
+    <Home />
   </div>
 );
 
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="home">
+    <h1>This is home page</h1>
+  </div>
+);
+
+export { Home };
